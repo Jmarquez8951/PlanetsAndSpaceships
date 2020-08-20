8ec3556f8c611386e66954f484ce9a03695c44a5
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-08-19 20:21:27 -0500

    got it to iterate through planets and give the probes that visited it

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PlanetsAndSpaceships
 {
@@ -23,9 +24,61 @@ namespace PlanetsAndSpaceships
 
             var rockyPlanets = planetList.GetRange(0, 4);
 
-            Console.WriteLine(string.Join(", ", rockyPlanets));
+            Console.Write("Rocky Planets: ");
+            Console.Write(string.Join(", ", rockyPlanets));
+            Console.WriteLine("");
 
             planetList.Remove("Pluto");
+
+            var spaceProbes = new Dictionary<string, List<string>>
+            {
+                {
+                    "Voyager 1", new List<string>
+                    {
+                        "Earth",
+                        "Jupiter",
+                        "Saturn",
+                    }
+                },
+                {
+                    "Voyager 2", new List<string>
+                    {
+                        "Earth",
+                        "Jupiter",
+                        "Saturn",
+                        "Uranus",
+                        "Neptune"
+                    }
+                },
+                {
+                    "Mariner 3", new List<string> 
+                    {
+                        "Earth",
+                        "Saturn",
+                        "Jupiter",
+                        "Uranus"
+                    }
+                },
+                {
+                     "Mariner 10", new List<string>
+                     {
+                         "Earth",
+                         "Neptune",
+                         "Venus",
+                         "Pluto"
+                     }
+                }
+            };
+
+            foreach (var planet in planetList)
+            {
+                var probes = spaceProbes
+                    .Where(probe => probe.Value.Contains(planet))
+                    .Select(probe => probe.Key);
+
+                Console.WriteLine($"{planet}: {string.Join(", ", probes)}");
+            }
+            
         }
     }
 }
